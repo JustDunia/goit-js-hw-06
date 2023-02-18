1e32b4cf609d812e2700868284fb03a67e3e5756
Justyna <justyna.sienkiewicz93@gmail.com>
2023-02-18 10:17:28 +0100

    tasks 1-3

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,14 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredientList = document.querySelector('#ingredients')
+
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments']
+
+const ingredientItems = []
+
+ingredients.forEach(el => {
+	const ingredientItem = document.createElement('li')
+	ingredientItem.textContent = el
+	ingredientItem.classList.add('item')
+	ingredientItems.push(ingredientItem)
+})
+
+ingredientList.append(...ingredientItems)
